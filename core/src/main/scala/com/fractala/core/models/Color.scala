9b75ef33b4f2970a180335322260eefb2a9be03b
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-03-16 14:50:13 +0100

    PR Suggestions: Added docstrings, changed folder structure, added an enum for storing Colors and Variable Grammar Symbol

---
+++ core/src/main/scala/com/fractala/core/models/Color.scala
@@ -0,0 +1,11 @@
+package com.fractala.core.models
+
+/**
+ * Represents available drawing colors used in the rendering process of the L-System (Lindenmayer system).
+ */
+enum Color {
+  case Black
+  case Red
+  case Green
+  case Blue
+}
