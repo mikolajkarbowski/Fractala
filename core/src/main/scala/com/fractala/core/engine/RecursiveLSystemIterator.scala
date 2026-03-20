87ebe8e8046168519c27a5aef6577751c70d6e12
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-03-20 12:27:41 +0100

    Added config and stack fields to LSystemIterator

--- core/src/main/scala/com/fractala/core/engine/RecursiveLSystemIterator.scala
+++ core/src/main/scala/com/fractala/core/engine/RecursiveLSystemIterator.scala
@@ -3,7 +3,23 @@ package com.fractala.core.engine
 import com.fractala.core.models.Symbol
 import com.fractala.core.traits.{Grammar, LSystemIterator}
 
-class RecursiveLSystemIterator extends LSystemIterator {
+import scala.collection.mutable
+
+
+case class Config(lineLength: Int,
+                  lineWidth: Int,
+                  turningAngle: Float,
+                  lineWidthIncrement: Int,
+                  lineWidthMultiplier: Int,
+                  turningAngleIncrement: Int)
+
+class RecursiveLSystemIterator(config: Config) extends LSystemIterator {
+  private var position: Vector[Double] = Vector(0.0, 0.0)
+  private var orientation: Vector[Double] = Vector(0.0, 1.0)
+  private var lineWidth = config.lineWidth
+  private var turningAngle = config.turningAngle
+  private var stack: mutable.Stack[(Vector[Double], Vector[Double])] = mutable.Stack()
+
   override def iterate(axiom: List[Symbol], grammar: Grammar, level: Int): Iterator[Symbol] = {
     if level == 0 then {
       axiom.iterator.filter(_ != Symbol.Variable)
